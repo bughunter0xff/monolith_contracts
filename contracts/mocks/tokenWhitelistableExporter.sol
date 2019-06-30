1d84d89e7fa9767b87e8921cb5fe1d4b498ad645
i-stam <i.stamelakos@gmail.com>
2019-06-30 20:41:46 +0100

    Expose only burnableTokens()

--- contracts/mocks/tokenWhitelistableExporter.sol
+++ contracts/mocks/tokenWhitelistableExporter.sol
@@ -5,34 +5,39 @@ import "../internals/ensResolvable.sol";
 
 contract TokenWhitelistableExporter is ENSResolvable, TokenWhitelistable {
 
-  constructor(address _ens_, bytes32 _tokenWhitelistName_) ENSResolvable(_ens_) TokenWhitelistable(_tokenWhitelistName_) public {}
 
-  function getTokenInfo(address _a) external view returns (string memory, uint256, uint256, bool, bool, bool, uint256) {
-      return _getTokenInfo(_a);
-  }
+    constructor(address _ens_, bytes32 _tokenWhitelistName_) ENSResolvable(_ens_) TokenWhitelistable(_tokenWhitelistName_) public {}
 
-  function getStablecoinInfo() external view returns (string memory, uint256, uint256, bool, bool, bool, uint256) {
-      return _getStablecoinInfo();
-  }
+    function getTokenInfo(address _a) external view returns (string memory, uint256, uint256, bool, bool, bool, uint256) {
+        return _getTokenInfo(_a);
+    }
 
-  function tokenAddressArray() external view returns (address[] memory) {
-      return _tokenAddressArray();
-  }
+    function getStablecoinInfo() external view returns (string memory, uint256, uint256, bool, bool, bool, uint256) {
+        return _getStablecoinInfo();
+    }
 
-  function updateTokenRate(address _token, uint _rate, uint _updateDate) external {
-      return _updateTokenRate(_token, _rate, _updateDate);
-  }
+    function tokenAddressArray() external view returns (address[] memory) {
+        return _tokenAddressArray();
+    }
 
-  function isTokenLoadable(address _a) external view returns (bool) {
-      return _isTokenLoadable(_a);
-  }
+    function burnableTokens() external view returns (address[] memory) {
+        return _burnableTokens();
+    }
 
-  function isTokenAvailable(address _a) external view returns (bool) {
-      return _isTokenAvailable(_a);
-  }
+    function updateTokenRate(address _token, uint _rate, uint _updateDate) external {
+        return _updateTokenRate(_token, _rate, _updateDate);
+    }
 
-  function isTokenBurnable(address _a) external view returns (bool) {
-      return _isTokenBurnable(_a);
-  }
+    function isTokenLoadable(address _a) external view returns (bool) {
+        return _isTokenLoadable(_a);
+    }
+
+    function isTokenAvailable(address _a) external view returns (bool) {
+        return _isTokenAvailable(_a);
+    }
+
+    function isTokenBurnable(address _a) external view returns (bool) {
+        return _isTokenBurnable(_a);
+    }
 
 }
